8ff84a2b248ab09883a7b818e46f617b34d7dc00
Mark Samwaiel <msamwaie@uwo.ca>
2024-11-03 21:33:59 -0500

    first commit

---
+++ src/Pets/Pet.java
@@ -0,0 +1,39 @@
+package Pets;
+
+public abstract class Pet {
+    private int currentHealth;
+    private int maxHealth;
+    private int sleepLevel;
+    private int hungerLevel;
+
+
+    private String name;
+
+    public Pet(String name) {
+        this.maxHealth = this.currentHealth = 100;
+    }
+
+    public int getHealth() {
+        return maxHealth;
+    }
+
+    public void setHealth(int health) {
+        this.maxHealth = health;
+    }
+
+    public int getSleepLevel() {
+        return sleepLevel;
+    }
+
+    public void setSleepLevel(int sleepLevel) {
+        this.sleepLevel = sleepLevel;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public void setHungerLevel(int hungerLevel) {
+        this.hungerLevel = hungerLevel;
+    }
+}
